4b5fb9eb221c126bd10eb047a4b13e78af6a4cef
kiang <kiange@gmail.com>
2021-10-15 23:41:30 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,177 @@
+var sidebar = new ol.control.Sidebar({element: 'sidebar', position: 'right'});
+var jsonFiles, filesLength, fileKey = 0;
+
+var projection = ol.proj.get('EPSG:3857');
+var projectionExtent = projection.getExtent();
+var size = ol.extent.getWidth(projectionExtent) / 256;
+var resolutions = new Array(20);
+var matrixIds = new Array(20);
+for (var z = 0; z < 20; ++z) {
+    // generate resolutions and matrixIds arrays for this WMTS
+    resolutions[z] = size / Math.pow(2, z);
+    matrixIds[z] = z;
+}
+
+var sidebarTitle = document.getElementById('sidebarTitle');
+var content = document.getElementById('sidebarContent');
+
+var appView = new ol.View({
+    center: ol.proj.fromLonLat([120.221507, 23.000694]),
+    zoom: 14
+});
+
+var vectorPoints = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        format: new ol.format.GeoJSON({
+            featureProjection: appView.getProjection()
+        }),
+        url: 'points.json'
+    }),
+    style: pointStyle
+});
+
+var baseLayer = new ol.layer.Tile({
+    source: new ol.source.WMTS({
+        matrixSet: 'EPSG:3857',
+        format: 'image/png',
+        url: 'https://wmts.nlsc.gov.tw/wmts',
+        layer: 'EMAP',
+        tileGrid: new ol.tilegrid.WMTS({
+            origin: ol.extent.getTopLeft(projectionExtent),
+            resolutions: resolutions,
+            matrixIds: matrixIds
+        }),
+        style: 'default',
+        wrapX: true,
+        attributions: '<a href="http://maps.nlsc.gov.tw/" target="_blank">國土測繪圖資服務雲</a>'
+    }),
+    opacity: 0.8
+});
+
+var map = new ol.Map({
+    layers: [baseLayer, vectorPoints],
+    target: 'map',
+    view: appView
+});
+
+map.addControl(sidebar);
+var pointClicked = false;
+map.on('singleclick', function (evt) {
+    pointClicked = false;
+    map.forEachFeatureAtPixel(evt.pixel, function (feature, layer) {
+        if (false === pointClicked) {
+            firstPosDone = true;
+            var p = feature.getProperties();
+            currentFeature = feature;
+            if (lastFeature) {
+                lastFeature.setStyle(pointStyle);
+            }
+
+            var lonLat = ol.proj.toLonLat(p.geometry.getCoordinates());
+            var message = '<table class="table table-dark">';
+            message += '<tbody>';
+            message += '<tr><th scope="row">藥局</th><td>' + p.name + '</td></tr>';
+            message += '<tr><th scope="row">負責人</th><td>' + p.owner + '</td></tr>';
+            message += '<tr><th scope="row">電話</th><td>' + p.phone + '</td></tr>';
+            message += '<tr><th scope="row">住址</th><td>' + p.address + '</td></tr>';
+            message += '<tr><td colspan="2">';
+            message += '<div class="btn-group-vertical" role="group" style="width: 100%;">';
+            message += '<a href="https://www.google.com/maps/dir/?api=1&destination=' + lonLat[1] + ',' + lonLat[0] + '&travelmode=driving" target="_blank" class="btn btn-info btn-lg btn-block">Google 導航</a>';
+            message += '<a href="https://wego.here.com/directions/drive/mylocation/' + lonLat[1] + ',' + lonLat[0] + '" target="_blank" class="btn btn-info btn-lg btn-block">Here WeGo 導航</a>';
+            message += '<a href="https://bing.com/maps/default.aspx?rtp=~pos.' + lonLat[1] + '_' + lonLat[0] + '" target="_blank" class="btn btn-info btn-lg btn-block">Bing 導航</a>';
+            message += '</div></td></tr>';
+            message += '</tbody></table>';
+            sidebarTitle.innerHTML = p.name;
+            currentFeature.setStyle(pointStyle);
+
+            lastFeature = currentFeature;
+            content.innerHTML = message;
+            sidebar.open('home');
+            pointClicked = true;
+        }
+    });
+});
+
+function pointStyle(f) {
+    var p = f.getProperties(), color, stroke, radius, pointCount;
+    if (f === currentFeature) {
+        stroke = new ol.style.Stroke({
+            color: '#000',
+            width: 5
+        });
+        radius = 25;
+    } else {
+        stroke = new ol.style.Stroke({
+            color: '#fff',
+            width: 2
+        });
+        radius = 15;
+    }
+    color = '#de690a';
+
+    return new ol.style.Style({
+        image: new ol.style.RegularShape({
+            radius: radius,
+            points: 3,
+            fill: new ol.style.Fill({
+                color: color
+            }),
+            stroke: stroke
+        })
+    })
+}
+
+var currentFeature = false;
+var lastFeature = false;
+
+var geolocation = new ol.Geolocation({
+    projection: appView.getProjection()
+});
+
+geolocation.setTracking(true);
+
+geolocation.on('error', function (error) {
+    console.log(error.message);
+});
+
+var positionFeature = new ol.Feature();
+
+positionFeature.setStyle(new ol.style.Style({
+    image: new ol.style.Circle({
+        radius: 6,
+        fill: new ol.style.Fill({
+            color: '#3399CC'
+        }),
+        stroke: new ol.style.Stroke({
+            color: '#fff',
+            width: 2
+        })
+    })
+}));
+
+var firstPosDone = false;
+geolocation.on('change:position', function () {
+    var coordinates = geolocation.getPosition();
+    positionFeature.setGeometry(coordinates ? new ol.geom.Point(coordinates) : null);
+    if (false === firstPosDone) {
+        appView.setCenter(coordinates);
+        firstPosDone = true;
+    }
+});
+
+new ol.layer.Vector({
+    map: map,
+    source: new ol.source.Vector({
+        features: [positionFeature]
+    })
+});
+
+$('#btn-geolocation').click(function () {
+    var coordinates = geolocation.getPosition();
+    if (coordinates) {
+        appView.setCenter(coordinates);
+    } else {
+        alert('目前使用的設備無法提供地理資訊');
+    }
+    return false;
+});
